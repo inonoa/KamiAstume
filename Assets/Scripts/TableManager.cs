c4e1bcc2a079a91badd8e19a97a9903b73eb4a15
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-07-01 05:19:19 +0900

    Xで机を生成しつづける

--- Assets/Scripts/TableManager.cs
+++ Assets/Scripts/TableManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TableManager : MonoBehaviour
 {
+    public GameObject table;
+    private float x;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class TableManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.X)){
+            Instantiate(table, new Vector3(x,0,0), Quaternion.identity);
+            x ++;
+        }
     }
 }
