4b963ba6c610beae9bfe5e1252f309cd91c484fd
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-07-01 08:38:18 +0900

    background and time

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text ui;
+    private int timeF = 60 * 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ui.text = "会議終了まで " + (timeF/60).ToString("00") + ":" + (timeF%60).ToString("00");
+        timeF --;
+    }
+}
