8db392319ff8b0e4244ba85a1496f8da502bfc55
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-07-01 20:35:35 +0900

    title2intro2game

--- Assets/Scripts/TableManager.cs
+++ Assets/Scripts/TableManager.cs
@@ -13,20 +13,17 @@ public class TableManager : MonoBehaviour
     public List<List<TableState>> tableStates = new List<List<TableState>>();
     public List<List<GameObject>> zenzais = new List<List<GameObject>>();
 
-    [HideInInspector]
     public float tableDistX = 1.6f;
-    [HideInInspector]
     public float tableDistY = -2.4f;
-    [HideInInspector]
     public int tableNumX = 7;
-    [HideInInspector]
-    public int tableNumY = 3;
-    [HideInInspector]
-    public Vector3 firstTableVec = new Vector3(-4.8f,2); // 書き換わってくれ
+    public int tableNumY = 2;
+    public Vector3 firstTableVec = new Vector3(-4.8f,2,1); // 書き換わってくれ
+    public Vector3 firstZenzaiVec;
 
     // Start is called before the first frame update
     void Start()
     {
+        firstZenzaiVec = firstTableVec + new Vector3(0,0,-1);
         //テーブル生成
         for(int j=0;j<tableNumY;j++){
             tables.Add(new List<GameObject>());
@@ -34,7 +31,7 @@ public class TableManager : MonoBehaviour
             tableStates.Add(new List<TableState>());
             for(int i=0;i<tableNumX;i++){
                 tables[j].Add(Instantiate(table, new Vector3(i*tableDistX,j*tableDistY,0) + firstTableVec, Quaternion.identity));
-                zenzais[j].Add(Instantiate(zenzai, new Vector3(i*tableDistX,j*tableDistY+0.5f,0) + firstTableVec, Quaternion.identity));
+                zenzais[j].Add(Instantiate(zenzai, new Vector3(i*tableDistX,j*tableDistY+0.5f,0) + firstZenzaiVec, Quaternion.identity));
                 zenzais[j][i].SetActive(false);
                 tableStates[j].Add(TableState.Empty);
             }
---
+++ Assets/Scripts/Title2IntroSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title2IntroSwitcher : MonoBehaviour
+{
+    public GameObject TitleObj;
+    public List<GameObject> intros = new List<GameObject>();
+    private int introPageNum;
+    private int page = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        introPageNum = intros.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z)){
+            if(page==0){
+                TitleObj.SetActive(false);
+                intros[0].SetActive(true);
+                page ++;
+            }else if(page<introPageNum-1){
+                intros[page-1].SetActive(false);
+                intros[page].SetActive(true);
+                page ++;
+            }else{
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+}
